90ef0e99f95e91dec04f3c141e10f932676d7238
Niageadon <46082748+Niageadon@users.noreply.github.com>
2019-07-04 15:20:50 +0300

    4. 8_4 done

---
+++ part 1/4/8_4.js
@@ -0,0 +1,30 @@
+/*
+Создайте функцию filterRangeInPlace(arr, a, b), которая получает массив с числами arr и удаляет из него все числа вне диапазона a..b. То есть, проверка имеет вид a ≤ arr[i] ≤ b. Функция должна менять сам массив и ничего не возвращать.
+
+Например:
+
+arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+alert( arr ); // массив изменился: остались [3, 1]
+*/
+
+function filterRangeInPlace(arr, a, b){
+  for (let i = 0; i< arr.length; i++){
+    if (arr[i] > b || arr[i] < a){
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+arr = [5, 3, 8, 1];
+
+let time = performance.now();
+// некий код
+filterRangeInPlace(arr, 1, 4);
+for (let i = 0; i < 1000; i++){
+  time = performance.now() - time;
+}
+console.log('Время выполнения = ', time);
+console.log(arr);
