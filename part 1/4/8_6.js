740304a0dbe520cccb18845ba1fc21fa9a2ac9f9
Niageadon <46082748+Niageadon@users.noreply.github.com>
2019-07-04 15:37:26 +0300

    4. 8_8 done

--- part 1/4/8_6.js
+++ part 1/4/8_6.js
@@ -14,6 +14,6 @@ alert( arr ); // HTML, JavaScript, CSS (без изменений)
 let arr = ["HTML", "JavaScript", "CSS"];
 let newArr =  arr.concat().sort((a, b)=>{
   return Math.random() - 0.5
-})
-console.log(arr)
-console.log(newArr)
+});
+console.log(arr);
+console.log(newArr);
--- part 1/4/8_7.js
+++ part 1/4/8_7.js
@@ -11,5 +11,5 @@ alert( arr ); // элементы в случайном порядке, напр
 let arr = [1, 2, 3, 4, 5];
 arr.sort((a, b)=>{
   return Math.random() - 0.5
-})
-console.log(arr)
+});
+console.log(arr);
