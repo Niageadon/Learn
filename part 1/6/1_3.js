3db59a888e3366593b82a3905bb233737a040697
Niageadon <46082748+Niageadon@users.noreply.github.com>
2019-07-10 13:29:40 +0300

    transfer

---
+++ part 1/6/1_3.js
@@ -0,0 +1,36 @@
+/*
+Вызовы (1) и (2) в примере ниже работают не так, как (3) и (4):
+
+ "use strict"
+
+var obj, method;
+
+obj = {
+  go: function() { alert(this); }
+};
+
+obj.go();            // (1) object
+
+(obj.go)();          // (2) object
+
+(method = obj.go)();      // (3) undefined
+
+(obj.go || obj.stop)(); // (4) undefined
+В чём дело? Объясните логику работы this.
+*/
+
+"use strict"
+
+var obj, method;
+
+obj = {
+  go: function() { alert(this); }
+};
+
+obj.go();            // (1) object
+
+(obj.go)();          // (2) object
+
+(method = obj.go)();      // (3) undefined
+console.log(method)
+(obj.go || obj.stop)(); // (4) undefined
