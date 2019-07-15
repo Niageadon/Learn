3021351f004d2fa57cb6f53541865b2a97d61ee5
Neageadon <legoog.tsop@gmail.com>
2019-07-15 21:39:54 +0300

    transfer

--- part 1/6/4_1.js
+++ part 1/6/4_1.js
@@ -1,81 +1,52 @@
 /*
 
-Напишите конструктор Calculator, который создаёт расширяемые объекты-калькуляторы.
+Вам попал в руки код объекта User, который хранит имя и фамилию в свойстве this.fullName:
 
-Эта задача состоит из двух частей, которые можно решать одна за другой.
+function User(fullName) {
+  this.fullName = fullName;
+}
 
-Первый шаг задачи: вызов calculate(str) принимает строку, например «1 + 2», с жёстко заданным форматом «ЧИСЛО операция ЧИСЛО» (по одному пробелу вокруг операции), и возвращает результат. Понимает плюс + и минус -.
+var vasya = new User("Василий Попкин");
+Имя и фамилия всегда разделяются пробелом.
 
-Пример использования:
+Сделайте, чтобы были доступны свойства firstName и lastName, причём не только на чтение, но и на запись, вот так:
 
-var calc = new Calculator;
+var vasya = new User("Василий Попкин");
 
-alert( calc.calculate("3 + 7") ); // 10
-Второй шаг – добавить калькулятору метод addMethod(name, func), который учит калькулятор новой операции. Он получает имя операции name и функцию от двух аргументов func(a,b), которая должна её реализовывать.
+// чтение firstName/lastName
+alert( vasya.firstName ); // Василий
+alert( vasya.lastName ); // Попкин
 
-Например, добавим операции умножить *, поделить / и возвести в степень **:
+// запись в lastName
+vasya.lastName = 'Сидоров';
 
-var powerCalc = new Calculator;
-powerCalc.addMethod("*", function(a, b) {
-  return a * b;
-});
-powerCalc.addMethod("/", function(a, b) {
-  return a / b;
-});
-powerCalc.addMethod("**", function(a, b) {
-  return Math.pow(a, b);
-});
-
-var result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
-Поддержка скобок и сложных математических выражений в этой задаче не требуется.
-Числа и операции могут состоять из нескольких символов. Между ними ровно один пробел.
-Предусмотрите обработку ошибок. Какая она должна быть – решите сами.
+alert( vasya.fullName ); // Василий Сидоров
+Важно: в этой задаче fullName должно остаться свойством, а firstName/lastName – реализованы через get/set. Лишнее дублирование здесь ни к чему.
 
 
 */
-function Calculator() {
-  function sum(a, b) {
-    return a + b
-  }
 
-  this.operators = [];
-  this.operators['+'] = {fun: sum};
-  this.calculate = function (val) {
-    let currentOperator;
-    for(let i in this.operators){
-      if(val.indexOf(i) !== -1) currentOperator = i;
-    }
-    let index = val.indexOf(currentOperator);
-    let a = +val.slice(0, index - 1);
-    let b = +val.slice(index + currentOperator.length, val.length);
-    return this.operators[currentOperator].fun(a, b)
-  };
-  this.addMethod = function(name, func){
-    for(let i in this.operators){
-      if(i === name) return 'Данная функция уже реализована';
-    }
-    this.operators[name] = {fun: func};
-    //console.log(this.operators)
-    //this.operators[name].fun = func;
-  }
+function User(fullName) {
+  this.fullName = fullName;
+  //this.firstName = 1;
 }
 
+var vasya = new User("Василий Попкин");
+console.log(vasya);
+Object.defineProperty(vasya, "firstName ", {
+  get: function() {
+    console.log(this.fullName);
+    var split = this.fullName.split(' ');
+    this.firstName = split[0];
+    this.surname = split[1];
+    return this.firstName//this.firstName;
+  },
+
+  set: function(value) {
+    var split = this.fullName.split(' ');
+
+  }
+});
 
 
-var powerCalc = new Calculator;
-powerCalc.addMethod("*", function(a, b) {
-  return a * b;
-});
-powerCalc.addMethod("/", function(a, b) {
-  return a / b;
-});
-powerCalc.addMethod("**", function(a, b) {
-  return Math.pow(a, b);
-});
-
-var result = powerCalc.calculate("2 + 3");
-console.log(result)
-console.log(powerCalc.calculate("2 * 3"))
-console.log(powerCalc.calculate("2 / 3"))
-console.log(powerCalc.calculate("2 ** 3"))
+console.log(vasya.firstName)
