14516e01ba89985b8b65bc6fd9b22f51302b3845
Niageadon <46082748+Niageadon@users.noreply.github.com>
2019-07-30 10:29:42 +0300

    10. 11_1 done

--- eng/part 1/1_1.js
+++ eng/part 1/1_1.js
@@ -2,6 +2,6 @@ function last(...list){
     return (list.length > 1)? list[list.length-1]: list[0][list[0].length-1]
 }
 
-console.log(last([1,2,3,4,5]))
-console.log(last("abcde"))
-console.log(last(1,"b",3,"d",5))
+console.log(last([1,2,3,4,5]));
+console.log(last("abcde"));
+console.log(last(1,"b",3,"d",5));
