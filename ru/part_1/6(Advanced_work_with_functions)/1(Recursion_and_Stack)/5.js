91edfbf6e26c239ad14be59ec3e6a95ebae0f3a6
Niageadon <legoog.tsop@gmail.com>
2019-08-07 18:59:01 +0300

    1_6 1_1 done

--- ru/part_1/6(Advanced_work_with_functions)/1(Recursion_and_Stack)/5.js
+++ ru/part_1/6(Advanced_work_with_functions)/1(Recursion_and_Stack)/5.js
@@ -21,18 +21,25 @@ let list = {
 };
 
 printList(list);
+printList_(list);
 
 function printList(list) {
   if(list.next){
     printList(list.next);
   }
-  console.log(list.value);
+  console.log('рекурсия: ', list.value);
 }
 
 function printList_(list) {
+  let array = [];
   let currentList = list;
   while (currentList){
-    console.log(currentList.value);
+    array.unshift(currentList.value);
     currentList = currentList.next;
   }
+  for(i of array){
+    console.log('цикл: ', i)
+  }
 }
+
+console.log('В этом примере преимущества рекурсии перед цикла очевидно.');
