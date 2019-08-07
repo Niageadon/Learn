30b5de7c771ddcdefed06d56c2b31ce0dfaf4e30
Niageadon <legoog.tsop@gmail.com>
2019-08-07 22:04:06 +0300

    transfer

---
+++ ru/part_1/6(Advanced_work_with_functions)/3(Closure)/6.js
@@ -0,0 +1,36 @@
+/* TODO
+
+У нас есть массив объектов, который нужно отсортировать:
+
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+Обычный способ был бы таким:
+
+// по имени (Ann, John, Pete)
+users.sort((a, b) => a.name > b.name ? 1 : -1);
+
+// по возрасту (Pete, Ann, John)
+users.sort((a, b) => a.age > b.age ? 1 : -1);
+Можем ли мы сделать его короче, скажем, вот таким?
+
+users.sort(byField('name'));
+users.sort(byField('age'));
+То есть, чтобы вместо функции, мы просто писали byField(fieldName).
+
+Напишите функцию byField, которая может быть использована для этого.
+
+* */
+function byField() {
+    return (type) => {}
+}
+
+
+let users = [
+    { name: "John", age: 20, surname: "Johnson" },
+    { name: "Pete", age: 18, surname: "Peterson" },
+    { name: "Ann", age: 19, surname: "Hathaway" }
+];
+users.sort(byField('name'));
