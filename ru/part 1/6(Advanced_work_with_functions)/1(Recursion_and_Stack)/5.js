b2e71a1ae5dcde6fbd17bdd0e812dd2975ebe159
Niageadon <legoog.tsop@gmail.com>
2019-08-07 16:35:32 +0300

    transfer

---
+++ ru/part 1/6(Advanced_work_with_functions)/1(Recursion_and_Stack)/5.js
@@ -0,0 +1,38 @@
+/*
+
+Выведите односвязный список из предыдущего задания Вывод односвязного списка в обратном порядке.
+
+Сделайте два решения: с использованием цикла и через рекурсию.
+
+* */
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+printList(list);
+
+function printList(list) {
+  if(list.next){
+    printList(list.next);
+  }
+  console.log(list.value);
+}
+
+function printList_(list) {
+  let currentList = list;
+  while (currentList){
+    console.log(currentList.value);
+    currentList = currentList.next;
+  }
+}
