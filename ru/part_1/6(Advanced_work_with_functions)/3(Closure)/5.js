3d0610142eb6086343d49486eef5a7992724ef91
Niageadon <legoog.tsop@gmail.com>
2019-08-07 21:43:50 +0300

    1_6 3_5 done

---
+++ ru/part_1/6(Advanced_work_with_functions)/3(Closure)/5.js
@@ -0,0 +1,42 @@
+/*
+
+У нас есть встроенный метод arr.filter(f) для массивов. Он фильтрует все элементы с помощью функции f.
+Если она возвращает true, то элемент добавится в возвращаемый массив.
+
+Сделайте набор «готовых к употреблению» фильтров:
+
+inBetween(a, b) – между a и b (включительно).
+inArray([...]) – находится в данном массиве.
+Они должны использоваться таким образом:
+
+arr.filter(inBetween(3,6)) – выбирает только значения межу 3 и 6 (включительно).
+arr.filter(inArray([1,2,3])) – выбирает только элементы, совпадающие с одним из элементов массива
+Например:
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+* */
+
+function inBetween(a, b){
+    return function (val) {
+        return ((val >= a) && (val <= b))
+    }
+}
+function inArray(arr){
+    return function (value) {
+        for (let i = 0; i< arr.length; i++){
+            if (arr[i] === value) return true
+        }
+        return false
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+console.log( arr.filter(inArray([1, 2, 10])) ); // 1,2
