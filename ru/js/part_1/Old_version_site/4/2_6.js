e30c39aaecff84365d232dcb7b9c483b2a17d2df
Niageadon <legoog.tsop@gmail.com>
2020-01-25 19:01:24 +0300

    started vue testing project from laracast

--- ru/js/part_1/Old_version_site/4/2_6.js
+++
@@ -1,8 +0,0 @@
-/*
-Напишите код для генерации случайного значения в диапазоне от 0 до max, не включая max.
-*/
-
-function randomNumber(maxValue){
-  return Math.random() * maxValue
-}
-console.log(randomNumber(10));
--- ru/js/part_1/Old_version_site/4/2_7.js
+++
@@ -1,8 +0,0 @@
-/*
-Напишите код для генерации случайного числа от min до max, не включая max.
-*/
-
-function randomNumber(minValue, maxValue){
-  return  minValue + Math.random() * (maxValue - minValue)
-}
-console.log(randomNumber(1, 2));
