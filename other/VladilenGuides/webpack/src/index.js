6f821e765ee3f689f5a860e3974478960e701a3e
Niageadon <legoog.tsop@gmail.com>
2020-02-01 23:34:20 +0300

    added json to project

--- other/VladilenGuides/webpack/src/index.js
+++ other/VladilenGuides/webpack/src/index.js
@@ -2,7 +2,9 @@ import {config} from "./modules/config";
 import service from './modules/service';
 import './modules/helloModule';
 import './styles/styles.css';
+import json from './assets/json'
 
-console.log(config);
+console.log('config', config);
+console.log('json', json);
 const serv = new service('helloBoba');
 serv.log();
