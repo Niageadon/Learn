4e5eeb07db04e63d22b262d6c3eb36ae0598120b
Niageadon <legoog.tsop@gmail.com>
2020-03-01 18:42:59 +0300

    ts variables type's and enum's

--- other/VladilenGuides/TS/1/1_variables.ts
+++ other/VladilenGuides/TS/1/1_variables.ts
@@ -1,2 +1,22 @@
+// BOOLEAN
+const isLoaded: boolean = false;
+let isDisabled: boolean = true;
+//isLoaded = true;
+isDisabled = false;
+
+//________________________________________________________
+// STRING
 const str: string = 'boba';
-console.log(str);
+
+//________________________________________________________
+// NUMBER
+const width: number = 720;
+let opacity: number = .5;
+const e: number = Math.E;
+let register: number = 0b101110100;
+register = 0xF1;
+
+//________________________________________________________
+// ARRAY
+let dishes: string[] = ['olive', 'borsh', 'kimchi'];
+
