9300663cf4cc1fcc963ef0ad94add00757d55a8a
Niageadon <legoog.tsop@gmail.com>
2020-03-15 13:52:28 +0300

    [webpack] added TS preset

--- other/VladilenGuides/webpack/webpack.config.js
+++ other/VladilenGuides/webpack/webpack.config.js
@@ -125,6 +125,22 @@ module.exports = {
                         ]
                     }
                 }
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-typescript'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }
             }
         ]
     },
