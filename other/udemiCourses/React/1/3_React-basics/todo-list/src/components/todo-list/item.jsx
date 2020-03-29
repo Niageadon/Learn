a47ed039d00bd2e63b7b5ae82b208d5bfd5c25ec
Niageadon <legoog.tsop@gmail.com>
2020-03-29 19:00:59 +0300

    [React-pro] 4_4 (setState work) done

--- other/udemiCourses/React/1/3_React-basics/todo-list/src/components/todo-list/item.jsx
+++ other/udemiCourses/React/1/3_React-basics/todo-list/src/components/todo-list/item.jsx
@@ -13,18 +13,30 @@ export default class Item extends React.Component {
 		})
  		console.log(this.state.isDone)
 	}
+	setImportance = () => {
+		this.setState({
+			isImportant: !this.state.isImportant
+		})
+	}
 	
 	render() {
 		const { label } = this.props;
-		let { isDone } = this.state;
+		let { isDone, isImportant } = this.state;
+		let style = 'todoList__item'
+		if(isDone) {
+			style += ' done'
+		}
+		if(isImportant) {
+			style += ' important'
+		}
 		
 		return (
-			<span className="todoList__item">
-				<span className={ isDone? 'done': ''} onClick={ this.onLabelClick }>
+			<span className={ style }>
+				<span className="todoList__item-label" onClick={ this.onLabelClick }>
 					{label}
 				</span>
 				<div className="todoList__item-controls">
-					<button type="button" className="todoList__item-controls-btn btn btn-outline-success">
+					<button onClick={this.setImportance} type="button" className="todoList__item-controls-btn btn btn-outline-success">
 						<i className="fa fa-exclamation"></i>
 					</button>
 					<button type="button" className="todoList__item-controls-btn btn btn-outline-danger">
