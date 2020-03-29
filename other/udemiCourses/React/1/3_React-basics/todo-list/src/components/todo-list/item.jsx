fb83fb1c5bc9695d5c8203610e3c44c97fcdddba
Niageadon <legoog.tsop@gmail.com>
2020-03-29 18:22:06 +0300

    [React-pro] 4_2 (event handling) done

--- other/udemiCourses/React/1/3_React-basics/todo-list/src/components/todo-list/item.jsx
+++ other/udemiCourses/React/1/3_React-basics/todo-list/src/components/todo-list/item.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import './item.scss'
 
 export default class Item extends React.Component {
-
+	onLabelClick = () => {
+		console.log(22)
+	}
+	
 	render() {
 		const {label} = this.props
 		
 		return (
 			<span className="todoList__item">
-				{label}
+				<span onClick={ this.onLabelClick }>
+					{label}
+				</span>
 				<div className="todoList__item-controls">
 					<button type="button" className="todoList__item-controls-btn btn btn-outline-success">
 						<i className="fa fa-exclamation"></i>
