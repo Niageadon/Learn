095e9a8e1abdbcb609e110ffaa98f5a45ded0173
Niageadon <legoog.tsop@gmail.com>
2020-04-04 15:29:13 +0300

    [React-pro] 6_5 - 6_6 (create swapi class) done

--- other/udemiCourses/React/React-pro/2-star-db/src/index.js
+++ other/udemiCourses/React/React-pro/2-star-db/src/index.js
@@ -1,12 +1,46 @@
-const fetchData = async (url) => {
-    try {
-        const a = await fetch(url)
-        return await a.json();
-    }
-    catch (e) {
-        throw e
-    }
+class Swapi {
+	baseUrl = 'https://swapi.co/api'
+	fetchData = async(url) => {
+		try {
+			const resp = await fetch(this.baseUrl + url)
+			if(!resp.ok) {
+				throw Error(`Could't fetch ${this.baseUrl + url}, received ${resp.status}`)
+			}
+			return await resp.json();
+		}
+		catch (e) {
+			throw e
+		}
+	}
+	async getAllPeople() {
+		const res = await this.fetchData('/people');
+		console.log(res)
+		return res.results;
+	}
+	async getPerson(id) {
+		return await this.fetchData(`/people/${id}`)
+	}
+	async getAllPlanets() {
+		const res = await this.fetchData('/planets');
+		console.log(res)
+		return res.results;
+	}
+	async getPlanet(id) {
+		return await this.fetchData(`/planets/${id}`)
+	}
+	async getAllStarships() {
+		const res = await this.fetchData('/starships');
+		console.log(res)
+		return res.results;
+	}
+	async getStarship(id) {
+		return await this.fetchData(`/starships/${id}`)
+	}
 }
-fetchData('https://swapi.co/api/people/1')
-    .then(console.log)
+const swapi = new Swapi();
+
+swapi.fetchData('/people/1')
+    //.then(console.log)
+
+swapi.getAllPeople().then(console.log)
 
