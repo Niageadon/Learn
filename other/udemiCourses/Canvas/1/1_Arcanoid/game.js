ffdfde25d23b16ceb47cc06a1c03a345b3c47057
Niageadon <legoog.tsop@gmail.com>
2020-04-18 17:48:44 +0300

    [Canvas] lesson 3 done

--- other/udemiCourses/Canvas/1/1_Arcanoid/game.js
+++ other/udemiCourses/Canvas/1/1_Arcanoid/game.js
@@ -1,5 +1,7 @@
 let game = {
     stx: null,
+    platform: null,
+    ball: null,
     sprites: {
         background: null,
         ball: null,
@@ -32,14 +34,26 @@ let game = {
     },
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 50, 50);
-        this.ctx.drawImage(this.sprites.platform, 50, 150);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     },
     start: async function() {
         this.init();
         await this.preload()
         this.run();
     }
+};
+
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+game.platform = {
+    x: 280,
+    y: 300
 };
 
 window.addEventListener("load", () => {
