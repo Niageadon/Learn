abcad4822e3a706687c575f57d5e541b798a28e0
Niageadon <legoog.tsop@gmail.com>
2020-04-18 17:20:51 +0300

    [Canvas] lesson 2 done

--- other/udemiCourses/Canvas/1/1_Arcanoid/game.js
+++ other/udemiCourses/Canvas/1/1_Arcanoid/game.js
@@ -1,12 +1,44 @@
 let game = {
-    start: function() {
+    stx: null,
+    sprites: {
+        background: null,
+        ball: null,
+        platform: null,
+    },
+    
+    init() {
         this.ctx = document.getElementById('mycanvas').getContext('2d');
-        let background = new Image();                    // создаём изображение
-        background.src = "./img/background.png";
-        console.log(background)
-        window.requestAnimationFrame(() => {    // Отрисовываем
-            this.ctx.drawImage(background, 0, 0);   // Намечаем его
+    },
+    preload() {
+        return new Promise(resolve => {
+            let index = 0;
+            for(let sprite in this.sprites) {
+                this.sprites[sprite] = new Image();
+                this.sprites[sprite].src = `./img/${sprite}.png`;
+                this.sprites[sprite].addEventListener('load', () => {
+                    index++;
+                    if(index >= Object.keys(this.sprites).length) {
+                        resolve();
+                    }
+                })
+            }
+        })
+        
+    },
+    run() {
+        window.requestAnimationFrame(() => {
+            this.render()
         });
+    },
+    render() {
+        this.ctx.drawImage(this.sprites.background, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 50, 50);
+        this.ctx.drawImage(this.sprites.platform, 50, 150);
+    },
+    start: async function() {
+        this.init();
+        await this.preload()
+        this.run();
     }
 };
 
