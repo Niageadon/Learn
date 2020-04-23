c59efb21df70cfb5ee869d992e84759351664b77
Niageadon <legoog.tsop@gmail.com>
2020-04-23 20:56:18 +0300

    [Canvas p.2] lesson 2 done

---
+++ other/udemiCourses/Canvas/1/2_Snake/game.js
@@ -0,0 +1,58 @@
+let game = {
+    ctx: null,
+    sprites: {
+        background: null,
+        body: null,
+        bomb: null,
+        cell: null,
+        food: null,
+        head: null,
+    },
+    init() {
+        return new Promise(resolve => {
+            this.ctx = document.getElementById('mycanvas').getContext('2d');
+            console.log(33)
+            //this.setEvents();
+            resolve()
+        })
+    },
+    preload() {
+        return new Promise(resolve => {
+            let index = 0;
+            const onImageLoad = () => {
+                index++;
+                if(index >= Object.keys(this.sprites).length) {
+                    resolve();
+                }
+            }
+            for(let sprite in this.sprites) {
+                this.sprites[sprite] = new Image();
+                this.sprites[sprite].src = `./img/${sprite}.png`;
+                this.sprites[sprite].addEventListener('load', onImageLoad)
+            }
+        })
+    },
+    start: async function() {
+        await this.init();
+        await this.preload();
+        //await this.create();
+        this.run();
+    },
+    run() {
+        window.requestAnimationFrame(() => {
+            
+            this.render();
+            
+            this.run();
+        });
+    },
+    render() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+    
+        this.ctx.drawImage(this.sprites.background, 0, 0);
+    }
+    
+    
+}
+
+game.start();
