ffd795244174701ce01e208a5bf4492b81e8f061
Niageadon <legoog.tsop@gmail.com>
2020-04-28 18:35:36 +0300

    [React-pro] 7_7 done

--- other/udemiCourses/React/React-pro/2-star-db/src/components/app/index.jsx
+++ other/udemiCourses/React/React-pro/2-star-db/src/components/app/index.jsx
@@ -5,18 +5,28 @@ import Person from '../person'
 import Planet from '../planet'
 import Starship from '../starship'
 import RandomPlanet from '../planet/random-planet'
+import ErrorIndicator from '../error/index'
 import './index.scss'
 
 export default class App extends React.Component{
 	state = {
 		itemId: 1,
-		
+		error: false
 	}
+	
+	componentDidCatch(error, errorInfo) {
+		this.setState({error: true})
+	}
+	
 	onItemSelected = (itemId) => {
 		this.setState({itemId})
 	}
 	
 	render() {
+		if(this.state.error) {
+			return <ErrorIndicator/>
+		}
+		
 		return (
 			<div className="app">
 				<Header/>
