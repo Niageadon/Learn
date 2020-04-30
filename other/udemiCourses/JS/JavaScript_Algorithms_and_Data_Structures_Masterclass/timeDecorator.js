8d209ee8515ec9954d07e27af18210f3668a5a25
Niageadon <legoog.tsop@gmail.com>
2020-04-30 20:01:53 +0300

    [Algorithms] lessons 6 - 8 done

--- other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/timeDecorator.js
+++ other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/timeDecorator.js
@@ -3,7 +3,7 @@ function getDelay (fun) {
 	const result = fun();
 	const endTime = new Date();
 	let delay = endTime - startTime;
-	delay = delay > 1e3? delay = delay / 1e3 + 's': delay + 'ms'
+	delay = delay > 1e3? delay = (delay / 1e3) + 's': delay + 'ms'
 	console.log({result, delay})
 	return({result, delay})
 }
