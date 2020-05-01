d7ef3c40f3acb678306574d7d9184f292a1a7c55
Niageadon <legoog.tsop@gmail.com>
2020-05-01 16:45:04 +0300

    [Algorithms] lessons 15 done

---
+++ other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/3_Analyzing_Performance_of_Arrays_and_Objects/2_Array-prices.js
@@ -0,0 +1,7 @@
+const array = {
+	insertionField: 'O(1) for push && O(N) for unshift',
+	removeField:    'O(1) for push && O(N) for unshift',
+	search:         'O(N)',
+	acess:          'O(1)',
+}
+
