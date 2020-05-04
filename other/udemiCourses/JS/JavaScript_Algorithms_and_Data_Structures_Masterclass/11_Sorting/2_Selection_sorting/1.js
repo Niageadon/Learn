a67bb453620a850a9302a7f27f34862283e22d0b
Niageadon <legoog.tsop@gmail.com>
2020-05-04 11:18:24 +0300

    [Algorithms] lessons <= 81 done

--- other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/11_Sorting/2_Selection_sorting/1.js
+++ other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/11_Sorting/2_Selection_sorting/1.js
@@ -18,10 +18,4 @@ function selectionSort(arr) {
 	return arr;
 }
 
-
-// O(N**2)
-const arr = new Array(10).fill(1).map(el => Math.floor(Math.random() * 5000));
-const arr1 = new Array(10).fill(1).map(el => Math.floor(Math.random() * 5000));
-//getDelay(() => selectionSort([3,2,1,5,7,3,3,32] || arr))
-
 console.log(selectionSort([1,4,6,3,21,645, 2]))
---
+++ other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/11_Sorting/3_Insertion_sorting/1.js
@@ -0,0 +1,45 @@
+const getDelay = require('../../timeDecorator');
+
+function insertionSort(arr) {
+	let temp = [arr[0]];
+	for (let i = 1; i < arr.length; i++) {
+		let currentVal = arr[i];
+			for(let j = 0; j < temp.length; j++) {
+				if(currentVal < temp[j]) {
+					insert(temp, j, currentVal);
+					break;
+				}
+			}
+			if(temp.length < i + 1) {
+				temp.push(currentVal)
+			}
+	}
+	return temp
+	
+	function insert(arr, index, val) {
+		for(let i = arr.length; i > index; i--) {
+			arr[i] = arr[i-1];
+		}
+		arr[index] = val;
+	}
+}
+
+function insertionSort1(nums) {
+	for (let i = 1; i < nums.length; i++) {
+		let j = i - 1
+		let temp = nums[i]
+		while (j >= 0 && nums[j] > temp) {
+			nums[j + 1] = nums[j]
+			j--
+		}
+		nums[j+1] = temp
+	}
+	return nums
+}
+
+const arr = new Array(100000).fill(1).map((el, index) => Math.floor(Math.random() * 5000 / index));
+const arr1 = new Array(100000).fill(1).map((el, index) => Math.floor(Math.random() * 5000 / index));
+
+
+getDelay(() => insertionSort(arr), false);
+getDelay(() => insertionSort1(arr1), false);
