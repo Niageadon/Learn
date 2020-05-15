0f4a2cc6238519b86851123b9c4c610f16f1c56d
Niageadon <legoog.tsop@gmail.com>
2020-05-15 20:29:49 +0300

    [Algorithms] lessons <= 218 done

--- other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/26_Graphs/1.js
+++ other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/26_Graphs/1.js
@@ -20,7 +20,22 @@ class Graph {
 		}
 		return this
 	}
+	removeEdge(a, b) {
+		const firstNode = this.adjacencyList[a];
+		const secondNode = this.adjacencyList[b];
+		let index;
+		if(firstNode.includes(b)) {
+			index = firstNode.findIndex(el => el === 'b')
+			firstNode.splice(index, 1);
+		}
+		if(secondNode.includes(a)) {
+			index = secondNode.findIndex(el => el === 'a')
+			secondNode.splice(index, 1);
+		}
+		return this
+	}
 }
 
 let g = new Graph();
 console.log(g.addEdge('boba', 'biba'))
+console.log(g.removeEdge('boba', 'biba'))
