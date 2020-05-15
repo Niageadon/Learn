2b1eec5c9695680e795dc80e3c243f8b3ef56eb2
Niageadon <legoog.tsop@gmail.com>
2020-05-15 19:26:15 +0300

    [Algorithms] lessons <= 204 done

--- other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/25_Hash_tables/1.js
+++ other/udemiCourses/JS/JavaScript_Algorithms_and_Data_Structures_Masterclass/25_Hash_tables/1.js
@@ -1,5 +1,5 @@
 class HashTable {
-	constructor(size = 53) {
+	constructor(size = 3) {
 		this.keyMap = new Array(size)
 	}
 	
@@ -22,6 +22,32 @@ class HashTable {
 		}
 		return undefined
 	}
+	keys() {
+		let values = [];
+		for(let i = 0; i < this.keyMap.length; i++) {
+			if(this.keyMap[i]) {
+				for(let j = 0; j < this.keyMap[i].length; j++) {
+					if(!values.includes(this.keyMap[i][j][0])) {
+						values.push(this.keyMap[i][j][0])
+					}
+				}
+			}
+		}
+		return values
+	}
+	values() {
+		let values = [];
+		for(let i = 0; i < this.keyMap.length; i++) {
+			if(this.keyMap[i]) {
+				for(let j = 0; j < this.keyMap[i].length; j++) {
+					if(!values.includes(this.keyMap[i][j][1])) {
+						values.push(this.keyMap[i][j][1])
+					}
+				}
+			}
+		}
+		return values
+	}
 	_hash(key) {
 		let total = 0;
 		let WEIRD_PRIME = 31;
@@ -39,7 +65,7 @@ function log() {
 }
 
 const ht = new HashTable();
-ht.set('hello', 'bb').set('pov', 'tii').set('cae', 'sdgs').set('dsd', 'ssdgf')
-console.log(ht.get('cae'))
+ht.set('hello', 'bb').set('pov', 'tii').set('cae', 'sdgs').set('dsd', 'ssdgf').set('dsdsd', 'dfdg')
 
-console.log(ht.keyMap)
+console.log(ht.keys())
+console.log(ht.values() )
