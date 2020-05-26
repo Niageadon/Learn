11596dd406fbf0b06e31c0c2958ae1074ca384c1
Niageadon <legoog.tsop@gmail.com>
2020-05-26 18:45:27 +0300

    [React] 11_4 done

--- other/udemiCourses/React/React-pro/4-redux/src/index.js
+++ other/udemiCourses/React/React-pro/4-redux/src/index.js
@@ -1,9 +1,24 @@
+import { createStore } from 'redux'
+
 const initState = 0;
-const reducer = (state, action) => {
-    if(action?.type === 'inc') {
-        return state + 1
+const reducer = (state = 0, action) => {
+    switch(action?.type) {
+        case 'inc': {
+            return state + 1
+        }
+        default: {
+            return state
+        }
     }
-    return 0
 }
 
 let state = reducer(initState, {type: 'inc'})
+
+
+const store = createStore(reducer)
+store.subscribe(() => {
+    console.log(store.getState())
+})
+store.dispatch({type: 'inc'});
+store.dispatch({type: 'inc'});
+
