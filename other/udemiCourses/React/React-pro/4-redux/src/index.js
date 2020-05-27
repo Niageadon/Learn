9b9581e602dfff3b27f756353f7de64e15827e98
Niageadon <legoog.tsop@gmail.com>
2020-05-27 21:20:23 +0300

    [React] 11_10 done

--- other/udemiCourses/React/React-pro/4-redux/src/index.js
+++ other/udemiCourses/React/React-pro/4-redux/src/index.js
@@ -1,18 +1,17 @@
-import { createStore } from 'redux'
+import { createStore, bindActionCreators } from 'redux'
 import reducer from './reducer'
 import {inc, dec} from './actions'
-const initState = 0;
-
-let state = reducer(initState, {type: 'inc'})
-
 
 const store = createStore(reducer);
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
-})
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec())
-})
+const { dispatch } = store
+const {incDispatch, decDispatch} = bindActionCreators({
+    incDispatch: inc,
+    decDispatch: dec
+}, dispatch);
+
+
+document.getElementById('inc').addEventListener('click',() => console.log(incDispatch));
+document.getElementById('dec').addEventListener('click', decDispatch);
 const update = () => {
     document.getElementById('counter').innerHTML = store.getState()
 }
