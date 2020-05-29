45aa37503bcdc4add640e9739aba5738bc788e62
Niageadon <legoog.tsop@gmail.com>
2020-05-29 21:52:07 +0300

    [React] 11_13 done

--- other/udemiCourses/React/React-pro/4-redux/src/components/counter.jsx
+++ other/udemiCourses/React/React-pro/4-redux/src/components/counter.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {bindActionCreators} from 'redux'
+import * as actions from '../actions'
 import { connect } from 'react-redux'
 const Counter = ({counter, inc, dec}) => {
     return (
@@ -15,5 +17,12 @@ const mapStateToProps = (state) => {
         counter: state
     }
 }
+const mapDispatchToProps = (dispatch) => {
+    const {inc, dec} = bindActionCreators(actions, dispatch);
+    return {
+        inc,//: () => dispatch({type: 'inc'}),
+        dec//: () => dispatch({type: 'dec'}),
+    }
+}
 
-export default connect(mapStateToProps)(Counter)
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
