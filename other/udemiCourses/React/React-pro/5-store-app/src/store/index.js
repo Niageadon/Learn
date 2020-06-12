403ebd5c8980c3118bf6cdb8b44063685808d554
Niageadon <legoog.tsop@gmail.com>
2020-06-12 13:06:48 +0300

    [React] 12_22 done

--- other/udemiCourses/React/React-pro/5-store-app/src/store/index.js
+++ other/udemiCourses/React/React-pro/5-store-app/src/store/index.js
@@ -1,5 +1,28 @@
-import {createStore} from 'redux'
+import {createStore, compose} from 'redux'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const stringEnhancer = (createStore) => (...args) => {
+	const store = createStore(...args);
+	const originalDispatch = store.dispatch;
+	store.dispatch = (action) => {
+		if(typeof action === 'string') {
+			return originalDispatch({type: action});
+		}
+		return originalDispatch(action);
+	}
+	
+	return store
+}
+const logEnhancer = (createStore) => (...args) => {
+	const store = createStore(...args);
+	const originalDispatch = store.dispatch;
+	store.dispatch = (action) => {
+		console.log(action)
+		return originalDispatch(action);
+	}
+	
+	return store
+}
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer))
 export default store
