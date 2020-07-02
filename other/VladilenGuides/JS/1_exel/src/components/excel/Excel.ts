8727062c2881114d2ded402d93557d0d9d375067
Niageadon <legoog.tsop@gmail.com>
2020-07-02 12:32:22 +0300

    [excel] lesson 33 done

--- other/VladilenGuides/JS/1_exel/src/components/excel/Excel.ts
+++ other/VladilenGuides/JS/1_exel/src/components/excel/Excel.ts
@@ -1,20 +1,20 @@
 import ExcelComponent from "@core/ExcelComponent.ts";
-import {$} from "@core/dom.ts";
+import {$, Dom} from "@core/dom.ts";
 type componentType = typeof ExcelComponent
 console.log(22, typeof ExcelComponent, ExcelComponent)
 export default class Excel {
-	$el: HTMLElement
+	$el: Dom
 	components: Array<componentType>//ExcelComponent[]
 	constructor(selector: string, options: Options) {
-		this.$el = document.querySelector(selector)
+		this.$el = $(selector)
 		this.components = options?.components ?? []
 	}
-	getRoot(): HTMLElement {
-		const $root: HTMLElement = $.create('div', 'excel')
+	getRoot(): Dom {
+		const $root: Dom = $.create('div', 'excel')
 		this.components.forEach(Component => {
-			const $el: HTMLElement = $.create('div', Component.className)
+			const $el: Dom = $.create('div', Component.className)
 			const component = new Component($el)
-			$el.innerHTML = component.toHTML()
+			$el.html(component.toHTML())
 			$root.append($el)
 		})
 
--- other/VladilenGuides/JS/1_exel/src/core/dom.ts
+++ other/VladilenGuides/JS/1_exel/src/core/dom.ts
@@ -1,4 +1,4 @@
-class Dom {
+export class Dom {
 	$el: HTMLElement
 	constructor(selector: string|HTMLElement) {
 		this.$el = typeof selector === "string"
@@ -6,16 +6,36 @@ class Dom {
 			: selector
 
 	}
+	html(html?: string): string|Dom {
+		if(typeof html === 'string') {
+			this.$el.innerHTML = html
+			return this
+		} else {
+			return this.$el.outerHTML.trim()
+		}
+	}
+	clear(): Dom {
+		this.html('')
+		return this
+	}
+	append({ $el }: Dom): Dom {
+		if(Element.prototype.append) {
+			this.$el.append($el)
+		} else {
+			this.$el.appendChild($el)
+		}
+		return this
+	}
 }
 
-export function $(selector) {
+export function $(selector: string|HTMLElement|undefined): Dom {
 	return new Dom(selector)
 }
 
-$.create = (tagName: string, classes?: string): HTMLElement => {
+$.create = (tagName: string, classes?: string): Dom => {
 	const el: HTMLElement = document.createElement(tagName)
 	if(classes) {
 		el.classList.add(classes)
 	}
-	return el
+	return $(el)
 }
