516cf8f15b93b02ca87eb7f59487b364423688fb
Niageadon <legoog.tsop@gmail.com>
2020-07-12 19:06:04 +0300

    [vue-ts] lessons 5-6 done

--- other/Vue/egghead/1_typeScript_in_vue/src/components/HelloTs.vue
+++ other/Vue/egghead/1_typeScript_in_vue/src/components/HelloTs.vue
@@ -1,15 +1,33 @@
 <template>
-	<h1>hello ts</h1>
+	<div>
+		<h1>hello ts</h1>
+		<my-checkbox
+			:title="checkbox.title"
+			:value="checkbox.value"
+			v-model="checkbox.checked"
+		>
+		</my-checkbox>
+	</div>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
-@Component({})
+import myCheckbox from "./myCheckbox.vue";
+@Component({
+	components: {
+		myCheckbox
+	}
+})
 export default class HelloTs extends Vue {
 	created() {
 		console.log('hello-ts created')
 	}
 	beforeRouteEnter(to: any, from: any, next: any) {
 		console.log('before route enter')
+	}
+	checkbox = {
+		title: 'my checkbox',
+		value: 'checkbox-id',
+		checked: true
 	}
 }
 
---
+++ other/Vue/egghead/1_typeScript_in_vue/src/components/myCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<input
+			type="checkbox"
+			:checked="checked"
+			:value="value"
+			@change="change"
+			placeholder="inp"
+		>
+		{{title}}
+	</div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Model } from "vue-property-decorator";
+@Component({})
+export default class MyCheckbox extends Vue {
+	@Prop() title: string | undefined
+	@Prop() value: string | undefined
+	@Prop()
+	@Model('change') checked: boolean | undefined
+	change(event: any) {
+		this.$emit('change', event.target.checked)
+	}
+}
+
+</script>
