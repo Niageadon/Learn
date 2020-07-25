1dfaa6298304bcbda49a01068b08f1794213d200
Niageadon <legoog.tsop@gmail.com>
2020-07-25 20:50:36 +0300

    [service-workers] lesson 22 is done

--- JS/2_ServiceWorkersAndPWA/2/web/js/sw.js
+++ JS/2_ServiceWorkersAndPWA/2/web/js/sw.js
@@ -1,13 +1,18 @@
 "use strict";
 
-// version = 1
+// version = 2
+let isOnline = true
+let isLoggedIn = false
+
 self.addEventListener('install', onInstall)
 self.addEventListener('activate', onActivate)
+self.addEventListener('message', onMessage)
 
 main().catch(console.error)
 ///
 async function main() {
 	console.log('my sw is starting...')
+	await sendMessage({ requestStatusUpdate: true })
 }
 async function onInstall(event) {
 	console.log('my sw installed...')
@@ -18,7 +23,27 @@ function onActivate(event) {
 }
 
 async function handleActivation() {
+	console.log(22, clients)
 	await clients.claim()
 	console.log('my sw activated...')
 	
+}
+
+async function sendMessage(msg) {
+	let allClients = await clients.matchAll({ includeUncontrolled: true })
+	return Promise.all(
+		allClients.map(client => {
+			let chan = new MessageChannel()
+			chan.port1.onmessage = onMessage
+			client.postMessage(msg, [chan.port2])
+		})
+	)
+}
+
+function onMessage({ data }) {
+	if (data.statusUpdate) {
+		isOnline = data.statusUpdate.isOnline
+		isLoggedIn = data.statusUpdate.isLoggedIn
+		console.log('sw status update', `is-online:${isOnline}`, `is-logged-in:${isLoggedIn}`)
+	}
 }
