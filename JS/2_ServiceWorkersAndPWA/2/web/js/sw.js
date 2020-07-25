2d57c089ef15062686a5b9676be28950f1cd555c
Niageadon <legoog.tsop@gmail.com>
2020-07-25 18:01:06 +0300

    [service-workers] lesson 18 is done

--- JS/2_ServiceWorkersAndPWA/2/web/js/sw.js
+++ JS/2_ServiceWorkersAndPWA/2/web/js/sw.js
@@ -1,3 +1,19 @@
 "use strict";
 
-// TODO
+// version = 1
+self.addEventListener('install', onInstall)
+self.addEventListener('activate', onActivate)
+
+main().catch(console.error)
+///
+async function main() {
+	console.log('my sw is starting...')
+}
+async function onInstall() {
+	console.log('my sw installed...')
+	self.skipWaiting()
+}
+async function onActivate() {
+	console.log('my sw activated...')
+
+}
