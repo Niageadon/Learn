5fe73aa7e36c84b43bbaf9d20ebb8952a98ae915
Niageadon <legoog.tsop@gmail.com>
2020-08-01 20:26:01 +0300

    [Dart] Lesson 5 is done

---
+++ Mobile/Dart/1_Dart-fundamental/code/5.dart
@@ -0,0 +1,13 @@
+void main() {
+  var firstName = 'boba';
+  String secondName = 'bibo';
+  var person = '$firstName $secondName';
+  var quote = 'If u can\'t explain it simply\n you boba';
+  String raw = r'Съешь [же] ещё этих мягких французских булок да выпей чаю';
+  var smile = '\u263a';
+
+  print(person);
+  print(quote);
+  print(raw);
+  print(smile);
+}
