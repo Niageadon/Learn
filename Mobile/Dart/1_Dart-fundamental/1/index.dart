108a98d640a0795f0f6ee1577a7ca99f8f71fec6
Niageadon <legoog.tsop@gmail.com>
2020-08-01 18:59:04 +0300

    [Dart] Lessons 1 - 3 are done

---
+++ Mobile/Dart/1_Dart-fundamental/1/index.dart
@@ -0,0 +1,19 @@
+void main() {
+  var age = 100500;
+  int age1 = 100500;
+  double pi = 3.14;
+  bool drawer = false;
+  dynamic val = 'key';
+    val = 2;
+    val = null;
+  const port = 8000;
+  final String name = 'bobo';
+
+  print(age);
+  print(age1);
+  print(pi);
+  print(drawer);
+  print(val);
+  print(port);
+  print('name: $name');
+}
