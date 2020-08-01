9c709a0c80e7becd1d7f9aea1d1c5bfb90d3bcdf
Niageadon <legoog.tsop@gmail.com>
2020-08-01 20:55:41 +0300

    [Dart] Lesson 6 is done

---
+++ Mobile/Dart/1_Dart-fundamental/code/6.dart
@@ -0,0 +1,28 @@
+void main() {
+
+  // Challenge Time! Strings and Runes
+
+  /* Create a single variable using three strings with the following text:
+   *
+   * I'm not creating the universe.
+   * I'm creating a model of the universe,
+   * which may or may not be true.
+   *
+   * Use single quotes for the first and third strings and
+   * double quotes for the second.
+   *
+   * Then print the variable to make sure the string looks right.
+   */
+  var qoute = 'I\'m not creating the universe.\n'
+      "I'm creating a model of the universe,"
+      'which may or may not be true.';
+  print(qoute);
+
+  /* Create a variable to hold the emoji for a dart hitting a target.
+   * Print the emoji.
+   *
+   * Hint: Use the hex value 1f3af.
+   */
+  var hit = '\u{1f3af}';
+  print(hit);
+}
