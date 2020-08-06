6d1209e6160febf9127db8320d7f49e249039fad
Niageadon <legoog.tsop@gmail.com>
2020-08-07 00:07:50 +0300

    [Flutter-beginner] lesson 6 done

--- Mobile/Flutter/1_flutter-beginner/readme.md
+++
@@ -1,6 +0,0 @@
-[src](https://www.udemy.com/course/flutters-beginners-course/)
-<br>courseAuthor: Bryan Cairns
-<br>startAt: 05.08.2020
-<br>endAt:
-<br>duration: 2h20m
-<br>level: beginner
--- Mobile/Flutter/1_flutter_beginner/button/lib/main.dart
+++ Mobile/Flutter/1_flutter_beginner/button/lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 class _State extends State<MyApp> {
+  String _value = 'my val';
+  void _onClick() {
+    setState(() {
+      _value = 'bobas';
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -16,14 +22,15 @@ class _State extends State<MyApp> {
         title: new Text('appbar-title'),
       ),
       body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Text('body-child-1-children')
-            ],
-          ),
-        )
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text(_value),
+                new RaisedButton(onPressed: _onClick)
+              ],
+            ),
+          )
       ),
     );
   }
