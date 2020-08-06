69dc291435e6092089471e85feef6edf24b681d2
Niageadon <legoog.tsop@gmail.com>
2020-08-06 23:45:11 +0300

    [Flutter-beginner] lessons 1 - 5 are done

---
+++ Mobile/Flutter/untitled/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('appbar-title'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text('body-child-1-children')
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
