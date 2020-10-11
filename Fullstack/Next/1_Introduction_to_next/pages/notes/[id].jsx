35b8f0b66c8df4e5582a1a496fe9d6a3232813e5
Niageadon <legoog.tsop@gmail.com>
2020-10-11 12:27:59 +0300

    [next] lessons 1 - 5 are done

---
+++ Fullstack/Next/1_Introduction_to_next/pages/notes/[id].jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Page = () => <div>notes page id</div>
+
+export default Page
---
+++ Fullstack/Next/1_Introduction_to_next/pages/notes/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Page = () => <div>notes page</div>
+
+export default Page
