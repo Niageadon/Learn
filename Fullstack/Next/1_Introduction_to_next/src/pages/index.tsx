c05602fe0a2512d7a69f8453bc694593607342db
Niageadon <legoog.tsop@gmail.com>
2020-10-15 23:52:56 +0300

    [next] lesson 20 is done

--- Fullstack/Next/1_Introduction_to_next/src/pages/index.tsx
+++ Fullstack/Next/1_Introduction_to_next/src/pages/index.tsx
@@ -5,9 +5,15 @@ import { jsx } from 'theme-ui'
 const Page = () => (
 	<div sx={{ height: 'calc(100vh - 50px)' }}>
 		<h1>index page</h1>
-		<Link href="/notes">
+		<Link href="/note">
 			<a>Notes</a>
 		</Link>
 	</div>
 )
 export default Page
+export function getStaticProps(context) {
+	console.log(11, context)
+	return {
+		props: {}
+	}
+}
