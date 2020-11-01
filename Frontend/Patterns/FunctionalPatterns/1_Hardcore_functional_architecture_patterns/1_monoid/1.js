0ab5b00119e640bf707c4abf9852bda6f69de542
Niageadon <legoog.tsop@gmail.com>
2020-11-01 16:48:17 +0300

    [fp] lesson 7 is done

--- Frontend/Patterns/FunctionalPatterns/1_Hardcore_functional_architecture_patterns/1_monoid/1.js
+++ Frontend/Patterns/FunctionalPatterns/1_Hardcore_functional_architecture_patterns/1_monoid/1.js
@@ -1,13 +1,24 @@
+// monoid  = semigroup + identity
 const sum = x => ({
 	x,
 	concat: other =>
 		sum(x + other.x)
 })
+sum.empty = () => sum(0)
+
+const amplify = x => ({
+	x,
+	concat: other =>
+		sum(x * other.x)
+})
+amplify.empty = () => amplify(1)
+
 const any = x => ({
 	x,
 	concat: other =>
 		any(x || other.x)
 })
+any.empty = () => any(false)
 
-const res = sum(3).concat(sum(5)) // w8 8
+const res = amplify.empty().concat(amplify(10)) // w8 8
 console.log(1, res)
