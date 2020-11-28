04ae4b7138ad33e226cce77a1e7a2e6a7e2c66d6
Niageadon <legoog.tsop@gmail.com>
2020-11-28 23:56:37 +0300

    [node] Lesson 3.11 is done

--- Backend/Node/0_Node-js_complete_guide/src/app.js
+++ Backend/Node/0_Node-js_complete_guide/src/app.js
@@ -7,7 +7,18 @@ const server = http.createServer((req, res) => {
 		case '/message': {
 			switch (method) {
 				case 'POST': {
-					fs.writeFileSync('message.txt', 'some text')
+					const body = []
+					req.on('data', (chunk) => {
+						console.log('chunk', chunk)
+						body.push(chunk)
+					})
+					req.on('end', () => {
+						const parsedBody = Buffer.concat(body).toString()
+						const value = parsedBody.split('=')[1]
+						fs.writeFileSync('message.txt', value)
+						
+						console.log('parsedBody', parsedBody)
+					})
 					res.statusCode = 302
 					res.setHeader('Location', '/')
 					return res.end()
