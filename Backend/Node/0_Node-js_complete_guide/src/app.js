d45e9d0fde8474617a2935b602b7e75e8357d0f0
Niageadon <legoog.tsop@gmail.com>
2020-11-28 20:50:40 +0300

    [node] Lesson 3.7 is done

--- Backend/Node/0_Node-js_complete_guide/src/app.js
+++ Backend/Node/0_Node-js_complete_guide/src/app.js
@@ -1,5 +1,9 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers)
+	res.setHeader('Content-Type', 'text/html')
+	res.statusCode = 200
+	res.write('<div>aa</div>')
+	res.end()
 })
 server.listen(3000)
