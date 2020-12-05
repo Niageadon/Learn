f93f84e23c252039ad91f6753855696f0e082e8b
Niageadon <legoog.tsop@gmail.com>
2020-12-05 21:37:34 +0300

    [node] Lesson 5.19 is done

--- Backend/Node/0_Node-js_complete_guide/src/app.ts
+++ Backend/Node/0_Node-js_complete_guide/src/app.ts
@@ -4,9 +4,9 @@ import bodyParser = require('body-parser')
 import { productRoutes, shopRoutes, route404 } from './routes'
 const app = express()
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(require('path').join(__dirname, 'public')))
+
 app.use('/', (req, res, next) => {
 	next()
 })
