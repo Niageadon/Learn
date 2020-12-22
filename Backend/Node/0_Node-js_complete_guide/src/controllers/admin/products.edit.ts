e0d9c707268d778ca88232671418384431e441d7
Niageadon <legoog.tsop@gmail.com>
2020-12-22 21:58:57 +0300

    [node] Lesson 9.11 is done

--- Backend/Node/0_Node-js_complete_guide/src/controllers/admin/products.edit.ts
+++ Backend/Node/0_Node-js_complete_guide/src/controllers/admin/products.edit.ts
@@ -1,9 +1,9 @@
 import { ProductService } from '../../services'
 export const get = (req, res, next) => {
-	const productId = req.body.productId
+	const productId = req.params.productId
 	new ProductService().get(productId, item => {
 		res.render('admin/products/edit', {
-			item,
+			product: item,
 			pageTitle: 'Edit product',
 			path: `/admin/products/${productId}/edit`
 	})
--- Backend/Node/0_Node-js_complete_guide/src/routes/admin.ts
+++ Backend/Node/0_Node-js_complete_guide/src/routes/admin.ts
@@ -2,7 +2,7 @@ import express = require('express')
 import { adminController } from '../controllers'
 const routes = express.Router()
 routes.get('/products/add', adminController.products.add.get)
-routes.get('/products/:productID/edit', adminController.products.edit.get)
+routes.get('/products/:productId/edit', adminController.products.edit.get)
 routes.get('/products', adminController.products.get)
 routes.post('/products', adminController.products.post)
 
