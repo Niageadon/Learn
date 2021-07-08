b8f1ddf48fc5763fea41d27cd30dcc16ed091d81
Niageadon <legoog.tsop@gmail.com>
2021-07-08 13:15:45 +0300

    [angular] lesson 21 is done

--- Frontend/Fameworks/Angular/0_angular/src/app/app.module.ts
+++ Frontend/Fameworks/Angular/0_angular/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component'
 import { CommonModule } from '@angular/common';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
--- Frontend/Fameworks/Angular/0_angular/src/app/post/post.component.ts
+++ Frontend/Fameworks/Angular/0_angular/src/app/post/post.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  templateUrl: './post.component.html'
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
