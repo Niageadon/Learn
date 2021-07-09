b8bd139d800b43ae37395452afe67964c2bd88da
Niageadon <legoog.tsop@gmail.com>
2021-07-09 15:00:01 +0300

    [angular] lesson 31 is done

--- Frontend/Fameworks/Angular/0_angular/1_directives/src/app/directives/style.directive.ts
+++ Frontend/Fameworks/Angular/0_angular/1_directives/src/app/directives/style.directive.ts
@@ -1,8 +1,10 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    renderer.setStyle(el.nativeElement, 'color', 'red')
+  }
 }
