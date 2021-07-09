d59751e5490d24bdda97cc038a2fb8d6b6f02a1d
Niageadon <legoog.tsop@gmail.com>
2021-07-09 12:59:07 +0300

    [angular] lesson 28 is done

--- Frontend/Fameworks/Angular/0_angular/src/app/app.component.ts
+++ Frontend/Fameworks/Angular/0_angular/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {IPost} from "./post/post.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   posts: IPost[] = [
     {
       title: 'title',
@@ -22,5 +23,11 @@ export class AppComponent {
     console.log(11, post)
     const index = this.posts.findIndex(el => el === post)
     this.posts.splice(index, 1)
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.posts[0].title = 'aboba'
+    }, 3000)
   }
 }
