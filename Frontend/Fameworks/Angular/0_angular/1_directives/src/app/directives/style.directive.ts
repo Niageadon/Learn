eb09c3c0a5da02275a3dbe7a81e8f95cb69770ec
Niageadon <legoog.tsop@gmail.com>
2021-07-09 16:49:18 +0300

    [angular] lesson 32 is done

--- Frontend/Fameworks/Angular/0_angular/1_directives/src/app/directives/style.directive.ts
+++ Frontend/Fameworks/Angular/0_angular/1_directives/src/app/directives/style.directive.ts
@@ -1,10 +1,21 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {
-    renderer.setStyle(el.nativeElement, 'color', 'red')
+  }
+
+  @HostListener('click', ['$event', "$event.target"]) onClick(event: Event, target: EventTarget) {
+    console.log(11, event, target)
+  }
+
+  @HostListener('mouseenter') onEnter(event: Event, target: EventTarget) {
+    this.renderer.setStyle(this.el.nativeElement, 'color', 'blue')
+  }
+
+  @HostListener('mouseleave') onLeave(event: Event, target: EventTarget) {
+    this.renderer.setStyle(this.el.nativeElement, 'color', 'red')
   }
 }
