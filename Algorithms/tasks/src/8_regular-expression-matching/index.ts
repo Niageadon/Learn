43f3ec3a1099927faeb1a0e1806f16cd5d635f25
Niageadon <legoog.tsop@gmail.com>
2021-11-21 17:00:22 +0300

    [async-js] lessons 25 - 38 are done

--- Algorithms/tasks/src/8_regular-expression-matching/index.ts
+++ Algorithms/tasks/src/8_regular-expression-matching/index.ts
@@ -20,6 +20,77 @@ export function isMatch(str: string, p: string): boolean {
 		}
 		return map;
 	}
+	let isMatch = false;
+	const map = generateMap();
+	let prevMultipleChar: string | undefined;
+	let charIndex = 0;
+	let entryIndex = 0;
+
+	while(1) {
+		const entryChar = map[entryIndex]?.v
+		const char = str[charIndex]
+		if (str[i] === prevMultipleChar || prevMultipleChar === '.') {
+			if (!map[index + 1]?.multiple && str[i] === map[index + 1]?.v && str[i + 1] !== str[i]) {
+				prevMultipleChar = map[index].v;
+				index++;
+				i--;
+				continue;
+			}
+			// Последний символ, но есть незадействованные вхождения
+			if ((i === str.length - 1) && isNoNextMultiple(map, i, entriesCount)) {
+				isMatch = false;
+				break;
+			}
+			if (i === str.length - 1) isMatch = true;
+			continue
+		}
+		else {
+			if (!mapChar) break
+
+			if (mapChar === char || mapChar === '.') {
+				if (map[index].multiple) {
+					prevMultipleChar = map[index].v;
+				} else {
+					prevMultipleChar = undefined
+				}
+				if (i === str.length - 1) {
+					isMatch = true;
+				} else if (i < str.length - 1) {
+					index++;
+				}
+				continue
+			} else {
+				isMatch = false;
+				i--;
+				index++;
+				continue;
+			}
+		}
+		if (i < str.length - 1) index++;
+	}
+
+
+	if (map[index + 1] !== undefined) {
+		isMatch = false;
+	}
+	return isMatch;
+};
+
+/*
+export function isMatch(str: string, p: string): boolean {
+	let entriesCount = 0;
+	const generateMap = (): Record<string, IWithMultiple> => {
+		let map: Record<string, IWithMultiple> = {};
+		for(let i = 0; i < p.length; i++) {
+			if (p[i] === '*') {
+				map[entriesCount - 1].multiple = true;
+			} else {
+				map[entriesCount] = { v: p[i] };
+				entriesCount++;
+			}
+		}
+		return map;
+	}
 
 	let isMatch = false;
 	const map = generateMap();
@@ -73,3 +144,4 @@ export function isMatch(str: string, p: string): boolean {
 	}
 	return isMatch;
 };
+* */
--- Frontend/JS/5_advanced-async-js/readme.md
+++ Frontend/JS/5_advanced-async-js/readme.md
@@ -1,5 +1,5 @@
 [src](https://frontendmasters.com/courses/advanced-async-js/introducing-advanced-asynchronous-javascript/)
 <br>duration: 3h30m
 <br>startAt: 07.11.2021
-<br>doneAt: 
+<br>doneAt: 21.11.2021
 
